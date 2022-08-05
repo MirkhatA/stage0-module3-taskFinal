c20bc1770822d78de5f533b28d199e48f6e6e824
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-08-05 17:02:10 +0600

    module 3

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,19 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void calculateSum(int number) {
+        String str = Integer.toString(number);
+
+        int result = 0;
+
+        for (char ch : str.toCharArray()) {
+            result += Character.getNumericValue(ch);
+        }
+
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        calculateSum(4567);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(number);
+
+        System.out.println(stringBuilder.reverse());
     }
 }
